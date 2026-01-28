7934a992a14d2a4d4149822b57d2018ef0272538
Jiriya <jiriya@Jiriyas-MacBook-Pro.local>
2026-01-27 20:12:21 -0600

    d

--- supabase-config.js
+++ supabase-config.js
@@ -2,4 +2,4 @@
 // Fill these from Supabase Project Settings -> API
 // IMPORTANT: Use the *anon public* key (NOT the sb_publishable_* key).
 window.SUPABASE_URL = "https://xjelqqauiwpsaotuspxs.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_secret_BpBruwmiQpidEBSO8iKsFQ_-wffd9Wy";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqZWxxcWF1aXdwc2FvdHVzcHhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0Nzg0NjMsImV4cCI6MjA4NTA1NDQ2M30.o-N8VIUOSfB6Umvm7ewXFSSr4ugBahbi1A5-YnF2o3s";
